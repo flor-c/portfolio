b2c38e98a5ff24ce780a18e890d9d09f5315fc22
= <=>
2024-02-12 13:31:03 +1300

    title, favicon and github actions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p className='text-slate-500'>Hi there, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold  text-pink-600'>Flor Cief</h1>
             <h2 className='text-4xl sm:text-7xl '>I'm a Frontend Developer.</h2>
-            <p className='text-slate-500 py-4 max-w-[700px]'>I'm a frontend developer specialasing in building and creating exceptional digital experiences. Currently, I'm focusing on building responsive web applications.</p>
+            <p className='text-slate-500 py-4 max-w-[700px]'>I'm a frontend developer specialising in building and creating exceptional digital experiences. Currently, I'm focusing on building responsive web applications.</p>
             <div>
                 <button className='group text-slate-400 border-2 border-slate-400 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 hover:text-slate-300'>
                   <Link to="work" smooth={true} duration={500} >
